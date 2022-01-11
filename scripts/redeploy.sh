c56851cad7ba3a8057c056c77e536e5b96083d78
Andriy Kalashnykov <AndriyKalashnykov@gmail.com>
2022-01-11 18:26:25 -0500

    updated accelerator and added TAP App Accelerator redeployment script

---
+++ scripts/redeploy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -x
+
+export KUBECONFIG=~/.kube/config
+SRV_SVC=$(kubectl get service -n accelerator-system acc-server| sed -n '2 p' | awk '{print $4}')
+
+
+CNT=$(tanzu acc list --server-url=http://$SRV_SVC } | grep -wc spring-on-k8s)
+
+if [ $cnt -eq 1 ]
+then
+    tanzu acc delete spring-on-k8s
+fi
+
+
+tanzu acc create spring-on-k8s --git-repository https://github.com/AndriyKalashnykov/spring-on-k8s.git --git-branch main
