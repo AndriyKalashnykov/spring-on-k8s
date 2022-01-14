c228a5d12f817bc1230fad172c52f3a89f1651e0
Andriy Kalashnykov <AndriyKalashnykov@gmail.com>
2022-01-14 16:14:26 -0500

    added App Accelerator resource

--- scripts/redeploy.sh
+++ scripts/redeploy.sh
@@ -2,6 +2,8 @@
 
 # set -x
 
+git cmp "updated accelert"
+
 export KUBECONFIG=~/.kube/config
 
 SRV_SVC=$(kubectl get service -n accelerator-system acc-server| sed -n '2 p' | awk '{print $4}')
@@ -10,8 +12,9 @@ CNT=$(tanzu acc list --server-url=http://$SRV_SVC } | grep -wc spring-on-k8s)
 
 if [ $CNT -eq 1 ]
 then
-    tanzu acc delete spring-on-k8s
+    tanzu acc delete spring-on-k8s --kubeconfig $HOME/.kube/config
 fi
 
+tanzu acc create spring-on-k8s --kubeconfig $HOME/.kube/config  --git-repository https://github.com/AndriyKalashnykov/spring-on-k8s.git --git-branch main
 
-tanzu acc create spring-on-k8s --git-repository https://github.com/AndriyKalashnykov/spring-on-k8s.git --git-branch main
+
