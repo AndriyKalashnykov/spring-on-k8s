5e4b9e16d3d46146e2e5c394a018aec1f0a2cf2f
Andriy Kalashnykov <AndriyKalashnykov@gmail.com>
2022-01-19 18:41:24 -0500

    updated accelerator

--- scripts/redeploy.sh
+++ scripts/redeploy.sh
@@ -10,7 +10,7 @@ export KUBECONFIG=$KC
 
 SRV_SVC=$(kubectl get service -n accelerator-system acc-server | sed -n '2 p' | awk '{print $4}')
 
-CNT=$(tanzu acc list --server-url=http://$SRV_SVC } | awk '{print $4}' | grep -wc spring-on-k8s)
+CNT=$(tanzu acc list --server-url=http://$SRV_SVC | awk '{print $4}' | grep -wc spring-on-k8s)
 
 if [ $CNT -gt 0 ]
 then
