13a6c69701706b40959a3688584304618ab0301a
Andriy Kalashnykov <AndriyKalashnykov@gmail.com>
2022-03-30 19:00:56 -0400

    Upgraded spring boot to 2.5.11

--- src/main/java/com/vmware/demos/springonk8s/api/rest/docs/SwaggerConfig.java
+++ src/main/java/com/vmware/demos/springonk8s/api/rest/docs/SwaggerConfig.java
@@ -1,7 +1,6 @@
 package com.vmware.demos.springonk8s.api.rest.docs;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
@@ -14,7 +13,6 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
-//@ComponentScan("com.vmware.demos.springonk8s.api.rest.controller")
 public class SwaggerConfig {
 
     @Bean
