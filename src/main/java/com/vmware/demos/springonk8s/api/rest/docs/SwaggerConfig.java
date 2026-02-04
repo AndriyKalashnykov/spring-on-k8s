b49aaa1a8ed80bc3250588769102f3f2b03751c5
Andriy Kalashnykov <AndriyKalashnykov@gmail.com>
2026-02-03 22:13:36 -0500

    fix: updates
    Signed-off-by: Andriy Kalashnykov <AndriyKalashnykov@gmail.com>

--- src/main/java/com/vmware/demos/springonk8s/api/rest/docs/SwaggerConfig.java
+++ src/main/java/com/vmware/demos/springonk8s/api/rest/docs/SwaggerConfig.java
@@ -1,42 +1,29 @@
 package com.vmware.demos.springonk8s.api.rest.docs;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 
 @Configuration
-@EnableSwagger2
 public class SwaggerConfig {
 
     @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .groupName("public-api")
-                .select()
-//                .apis(RequestHandlerSelectors.any())
-//                .apis(Predicates.not(RequestHandlerSelectors.basePackage("org.springframework.boot")))
-//                .paths(Predicates.not(PathSelectors.regex("/error")))
-                .apis(RequestHandlerSelectors.basePackage("org.springframework.boot").negate())
-                .paths(PathSelectors.any())
-                .build()
-                .apiInfo(apiInfo());
+    public OpenAPI api() {
+        return new OpenAPI()
+                .info(apiInfo());
     }
 
-    private ApiInfo apiInfo() {
-        return new ApiInfoBuilder()
+    private Info apiInfo() {
+        return new Info()
                 .title("REST + Swagger UI")
                 .description("REST + Swagger UI sample app")
-                .termsOfServiceUrl("github")
-                .license("Apache 2.0")
-                .licenseUrl("")
-                .version("1.0")
-                .build();
+                .termsOfService("github")
+                .license(new License()
+                        .name("Apache 2.0")
+                        .url(""))
+                .version("1.0");
     }
 }
